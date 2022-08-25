3772124cda1bb4cb06bcfa98f9a51d8bca779275
kimkibon <darkhalu@naver.com>
2022-08-25 15:43:45 +0900

    first commit

---
+++ polstory/src/test/java/com/project/polstory/PolstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.polstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
